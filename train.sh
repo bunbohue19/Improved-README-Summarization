641e7bce602c16eca2025f6189014c8863cd2e4b
Nguyễn Đình Lộc <95389934+bunbohue19@users.noreply.github.com>
2023-11-29 11:46:29 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ export TRANSFORMERS_CACHE=~/READMESum/hf-pretrained-checkpoints/
 export TRANSFORMERS_CACHE=/media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Loc/Improved-README-Summarization/hf-pretrained-checkpoints/
 
 # Specify the device
-export CUDA_VISIBLE_DEVICES="0,1"
+export CUDA_VISIBLE_DEVICES="0"
 
 # Disable tokenizers parallelism
 export TOKENIZERS_PARALLELISM=false
