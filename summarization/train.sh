fe283433cc73279ee838b8d9b1b5e24dca619b84
bunbohue19 <bunbohue1906@outlook.com>
2023-12-09 00:05:22 +0700

    update changes

--- summarization/train.sh
+++ summarization/train.sh
@@ -3,7 +3,7 @@ huggingface-cli login --token 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
 
 # Specify the cache directory
 # export TRANSFORMERS_CACHE=~/READMESum/hf-pretrained-checkpoints/
-# export TRANSFORMERS_CACHE=/media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Loc/Improved-README-Summarization/hf-pretrained-checkpoints/
+# export TRANSFORMERS_CACHE=/media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Loc/hf-pretrained-checkpoints/
 # export TRANSFORMERS_CACHE=../../hf-pretrained-checkpoints/
 
 # Specify the device
